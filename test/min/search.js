dd3fbfd89dd3438e8897ad5effc548825dd23d2b
deathsteps <lovely_dreamer@126.com>
2014-10-09 15:26:06 +0800

    fix issue #4

---
+++ test/min/search.js
@@ -0,0 +1,4 @@
+
+/**BuildTime: Thu Oct 09 2014 14:40:19 GMT+0800 (中国标准时间)
+ *If you found any bug, please mail to shiz@ctrip.com*/
+define(function(require,exports){exports.getCode=function(){return"search"}});
--- test/min/test.js
+++ test/min/test.js
@@ -1,30 +1,13 @@
-define(function (require, exports, module) {
+/**BuildTime: Sun Sep 28 2014 18:34:36 GMT+0800 (中国标准时间)
+ *If you found any bug, please mail to shiz@ctrip.com*/
+define("tools/tool",[],function(require,exports){exports.multiply=function(n,i){return n*i}});;
+/**BuildTime: Sun Sep 28 2014 18:34:57 GMT+0800 (中国标准时间)
+ *If you found any bug, please mail to shiz@ctrip.com*/
+define("mods/quickview",[],function(require,exports,module){function n(){}n.prototype.show=function(n){return n},module.exports=n});;
 
-	var Tools = require('tools/tool');
-	var QuickView  = require('mods/quickview');
-	
-	QUnit.test( "Require synchronously", function( assert ) {
-		assert.equal(typeof Tools, 'object', 'Import tools/tool successfully');
-		assert.equal(Tools.multiply(2, 3), 6, 'Tools.multiply excutes successfully');
-
-		assert.equal(typeof QuickView, 'function', 'Import mods/quickview successfully');
-		assert.equal(new QuickView().show('QuickView'), 'QuickView', 'QuickView.show excutes successfully');
-	});
-
-	var searcher = require('code/search');
-	
-	QUnit.test( "Compress dependent files while compile the specified file", function( assert ) {
-		assert.equal(typeof searcher, 'object', 'Import code/search successfully');
-		assert.equal(searcher.getCode(), 'search', 'searcher.getCode excutes successfully');
-	});
-	
-	QUnit.asyncTest( "Require asynchronously", function( assert ) {
-		expect( 2 );
-		require.async('tools/tab', function (Tab){
-			assert.equal(typeof Tab, 'function', 'Import tools/tab asynchronously successfully');
-			assert.equal(new Tab().switchTo(2), 2, 'Tab.switchTo excutes successfully');
-			QUnit.start();
-		});
-	});
-	
-});
+/**BuildTime: Thu Oct 09 2014 14:40:19 GMT+0800 (中国标准时间)
+ *If you found any bug, please mail to shiz@ctrip.com*/
+define("webresource/search",[],function(require,exports){exports.getCode=function(){return"search"}});;
+/**BuildTime: Thu Oct 09 2014 14:40:10 GMT+0800 (中国标准时间)
+ *If you found any bug, please mail to shiz@ctrip.com*/
+define(function(require){var e=require("tools/tool"),s=require("mods/quickview");QUnit.test("Require synchronously",function(c){c.equal(typeof e,"object","Import tools/tool successfully"),c.equal(e.multiply(2,3),6,"Tools.multiply excutes successfully"),c.equal(typeof s,"function","Import mods/quickview successfully"),c.equal((new s).show("QuickView"),"QuickView","QuickView.show excutes successfully")});var c=require("webresource/search");QUnit.test("Compress dependent files while compiling the specified file",function(e){e.equal(typeof c,"object","Import webresource/search successfully"),e.equal(c.getCode(),"search","searcher.getCode excutes successfully")}),QUnit.asyncTest("Require asynchronously",function(e){expect(2),require.async("tools/tab",function(s){e.equal(typeof s,"function","Import tools/tab asynchronously successfully"),e.equal((new s).switchTo(2),2,"Tab.switchTo excutes successfully"),QUnit.start()})})});
