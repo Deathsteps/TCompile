401010f15d51e06ee75b251560a5cde9bb0f3a99
deathsteps <lovely_dreamer@126.com>
2014-11-03 17:42:03 +0800

    add handlebars template compiling support

---
+++ test/src/templateTest.js
@@ -0,0 +1,41 @@
+define(function (require, exports, module) {
+	var data1 = {
+		title: "Normal Title",
+		body: "<p>This is a post about &lt;p&gt; tags</p>"
+	};
+	var data2 = {
+		people: [
+			{firstName: "abc", lastName: "123"},
+			{firstName: "def", lastName: "456"}
+		]
+	}
+	var data3 = {
+		title: "View Title",
+		body: "View Body",
+		people: [
+			{firstName: "John", lastName: "Smith"},
+			{firstName: "Lily", lastName: "Baker"}
+		]
+	};
+	
+	require('webresource/templateTest/views.js');
+	
+	QUnit.test( "Handlebars templates precompiling", function( assert ) {
+
+		var htmls = [
+			Handlebars.templates["normal"](data1),
+			Handlebars.templates["customHelper"](data2),
+			Handlebars.templates["view"](data3)
+		];
+		var testStr = [
+			'<h1>Normal Title</h1>',
+			'<p>abc - 123</p>',
+			'<p>Lily - Baker</p>'
+		]
+
+		assert.ok(htmls[0].indexOf(testStr[0]) != -1, 'Normal template works.');
+		assert.ok(htmls[1].indexOf(testStr[1]) != -1, 'Tools.multiply excutes successfully');
+		assert.ok(htmls[2].indexOf(testStr[2]) != -1, 'Import mods/quickview successfully');
+	});
+	
+});
